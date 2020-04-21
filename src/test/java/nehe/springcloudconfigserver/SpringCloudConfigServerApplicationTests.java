464cbf051db0f76a22d717359dc64107116aa48c
= <kamolunehemiah@gmail.com>
2020-04-21 20:51:30 +0300

    first commit

---
+++ src/test/java/nehe/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nehe.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
